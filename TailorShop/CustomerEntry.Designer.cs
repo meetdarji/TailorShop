c7c40bfb8c2d9ac7b1c12277e6b30ba487783262
meet-dashtechinc <meet.dashtechinc@gmail.com>
2019-04-26 18:49:40 +0530

    Customer Entery Form

---
+++ TailorShop/CustomerEntry.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace TailorShop
+{
+    partial class CustomerEntry
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(47, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ગ્રાહક નું નામ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(47, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "ગ્રાહક મોબાઇલ નંબર";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(47, 108);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "ગ્રાહક સરનામું";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(47, 189);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "ગ્રાહક ઇમેઇલ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(47, 229);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(61, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "ગ્રાહક લિંગ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(178, 39);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(368, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(178, 74);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(178, 108);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(368, 60);
+            this.textBox3.TabIndex = 7;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(178, 189);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(368, 20);
+            this.textBox4.TabIndex = 8;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton1.Location = new System.Drawing.Point(178, 229);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(51, 24);
+            this.radioButton1.TabIndex = 9;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "પુરુષ";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton2.Location = new System.Drawing.Point(289, 229);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(42, 24);
+            this.radioButton2.TabIndex = 10;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "સ્ત્રી";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(304, 270);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 26);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "નવું ગ્રાહક ઉમેરો";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(436, 270);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(110, 26);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "ગ્રાહક અપડેટ કરો";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(38, 315);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(695, 150);
+            this.dataGridView1.TabIndex = 13;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(178, 12);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 15;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(47, 12);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(102, 20);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "ગ્રાહક આઈડી નંબર";
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(178, 270);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 26);
+            this.button3.TabIndex = 16;
+            this.button3.Text = "નવું";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(578, 270);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 26);
+            this.button4.TabIndex = 17;
+            this.button4.Text = "કાઢી નાખો";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // CustomerEntry
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(776, 494);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "CustomerEntry";
+            this.Text = "ગ્રાહક ખાતું";
+            this.Load += new System.EventHandler(this.CustomerEntry_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
---
+++ TailorShop/CustomerEntry.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace TailorShop
+{
+    public partial class CustomerEntry : Form
+    {
+        public CustomerEntry()
+        {
+            InitializeComponent();
+            dataGridView1.CellClick += dataGridView1_CellClick;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string gender = "";
+
+                if (radioButton1.Checked)
+                {
+                    gender = "પુરુષ";
+                }
+                else if(radioButton2.Checked)
+                {
+                    gender = "સ્ત્રી";
+                }
+                else
+                {
+                    gender = "";
+                }
+                SqlConnection con = new SqlConnection("Data Source=MEET-LP;Initial Catalog=TailorShop;Integrated Security=SSPI");
+                SqlCommand cmd;
+                con.Open();
+                string s = "insert into tblCustomer values(@CustomerName,@CustomerMobileNo,@CustomerAddress,@EmailId,@Gender,@Status)";
+                cmd = new SqlCommand(s, con);
+                cmd.Parameters.AddWithValue("@CustomerName", textBox1.Text);
+                cmd.Parameters.AddWithValue("@CustomerMobileNo", textBox2.Text);
+                cmd.Parameters.AddWithValue("@CustomerAddress", textBox3.Text);
+                cmd.Parameters.AddWithValue("@EmailId", textBox4.Text);
+                cmd.Parameters.AddWithValue("@Gender", gender);
+                cmd.Parameters.AddWithValue("@Status", "1");
+                cmd.CommandType = CommandType.Text;
+                int i = cmd.ExecuteNonQuery();
+                con.Close();
+                MessageBox.Show(i + " Row(s) Inserted ");
+                gridbind();
+                clrtextbox();
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+            
+        }
+
+        public void gridbind()
+        {
+            SqlConnection con = new SqlConnection("Data Source=MEET-LP;Initial Catalog=TailorShop;Integrated Security=SSPI");
+            SqlCommand cmd;
+            string s = "select CustomerID,CustomerName,CustomerMobileNo,CustomerAddress,EmailId,Gender from tblCustomer where Status='1'";
+            cmd = new SqlCommand(s, con);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            con.Open();
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+            con.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string gender = "";
+
+                if (radioButton1.Checked)
+                {
+                    gender = "પુરુષ";
+                }
+                else if (radioButton2.Checked)
+                {
+                    gender = "સ્ત્રી";
+                }
+                else
+                {
+                    gender = "";
+                }
+                SqlConnection con = new SqlConnection("Data Source=MEET-LP;Initial Catalog=TailorShop;Integrated Security=SSPI");
+                SqlCommand cmd;
+                con.Open();
+                string s = "UPDATE tblCustomer SET CustomerName=@CustomerName,CustomerMobileNo=@CustomerMobileNo,CustomerAddress=@CustomerAddress,EmailId=@EmailId,Gender=@Gender  WHERE CustomerID=" + textBox5.Text+"";
+                cmd = new SqlCommand(s, con);
+                cmd.Parameters.AddWithValue("@CustomerName", textBox1.Text);
+                cmd.Parameters.AddWithValue("@CustomerMobileNo", textBox2.Text);
+                cmd.Parameters.AddWithValue("@CustomerAddress", textBox3.Text);
+                cmd.Parameters.AddWithValue("@EmailId", textBox4.Text);
+                cmd.Parameters.AddWithValue("@Gender", gender);
+                cmd.CommandType = CommandType.Text;
+                int i = cmd.ExecuteNonQuery();
+                con.Close();
+                MessageBox.Show(i + " Row(s) Updated ");
+                gridbind();
+                clrtextbox();
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+
+        private void CustomerEntry_Load(object sender, EventArgs e)
+        {
+            gridbind();
+            textBox1.Enabled = false;
+            textBox2.Enabled = false;
+            textBox3.Enabled = false;
+            textBox4.Enabled = false;
+            textBox5.Enabled = false;
+            radioButton1.Enabled = false;
+            radioButton2.Enabled = false;
+
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button4.Enabled = false;
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int index = e.RowIndex;// get the Row Index
+            DataGridViewRow selectedRow = dataGridView1.Rows[index];
+
+            textBox5.Text = selectedRow.Cells[0].Value.ToString();
+            textBox1.Text = selectedRow.Cells[1].Value.ToString();
+            textBox2.Text = selectedRow.Cells[2].Value.ToString();
+            textBox3.Text = selectedRow.Cells[3].Value.ToString();
+            textBox4.Text = selectedRow.Cells[4].Value.ToString();
+
+            string gender = selectedRow.Cells[5].Value.ToString();
+
+            if (gender == "પુરુષ")
+            {
+                radioButton1.Checked = true;
+            }
+            else
+            {
+                radioButton2.Checked = true;
+            }
+            enabletextbox();
+            button1.Enabled = false;
+            button2.Enabled = true;
+            button4.Enabled = true;
+
+        }
+
+        public void clrtextbox()
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            textBox5.Text = "";
+            radioButton1.Checked = false;
+            radioButton2.Checked = false;
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            clrtextbox();
+            enabletextbox();
+
+            button1.Enabled = true;
+            button2.Enabled = false;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            var confirmResult = MessageBox.Show("Are you sure to delete this Customer ??",
+                                     "Confirm Delete!!",
+                                     MessageBoxButtons.YesNo);
+            if (confirmResult == DialogResult.Yes)
+            {
+                try
+                {
+                    SqlConnection con = new SqlConnection("Data Source=MEET-LP;Initial Catalog=TailorShop;Integrated Security=SSPI");
+                    SqlCommand cmd;
+                    con.Open();
+                    string s = "UPDATE tblCustomer SET Status=@Status WHERE CustomerID=" + textBox5.Text + "";
+                    cmd = new SqlCommand(s, con);
+                    cmd.Parameters.AddWithValue("@Status", "0");
+                    cmd.CommandType = CommandType.Text;
+                    int i = cmd.ExecuteNonQuery();
+                    con.Close();
+                    MessageBox.Show(i + " Row(s) Deleted ");
+                    gridbind();
+                    clrtextbox();
+                }
+                catch (Exception ex)
+                {
+
+                    throw;
+                }
+            }
+            else
+            {
+                // If 'No', do something here.
+            }
+        }
+
+        public void enabletextbox()
+        {
+            textBox1.Enabled = true;
+            textBox2.Enabled = true;
+            textBox3.Enabled = true;
+            textBox4.Enabled = true;
+            textBox5.Enabled = true;
+            radioButton1.Enabled = true;
+            radioButton2.Enabled = true;
+        }
+
+    }
+}
